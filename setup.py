6b987ba6e9c22e1a6fc90a2fdf4d61927c604d0d
Marko Springfeldt <marko.springfeldt@googlemail.com>
2018-12-05 15:25:20 +0100

    Bumped to correct version 2.4.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
       name='JSON Schema OOP',
-      version='2.2.0',
+      version='2.4.0',
       description='OOP JSON Schema lets you write JSON-Schema classes and inherits them',
       author='Trung Phan',
       author_email='info@level96.de',
